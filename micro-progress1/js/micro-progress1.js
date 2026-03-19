561df03077dda6fe13662365f3040c69eff26bc9
delaneyyurbann <durban@sfsu.edu>
2026-03-19 12:57:31 -0700

    3.19

--- micro-progress1/js/micro-progress1.js
+++ micro-progress1/js/micro-progress1.js
@@ -1,17 +1,20 @@
+let planeImg;
 let globeImg;
 let currentFact = "Click on locations on the globe to begin your adventure!";
 let unlockedCount = 0;
 
 const dots = [
-  { name: "Paris",  x: 10, y: -140, fact: "The Eiffel Tower expands slightly in summer heat.", unlocked: false },
-  { name: "Tangier",  x:  -10,  y: -90, fact: "On a clear day in Tangier, you can see Europe from Africa, Spain is only 9 miles away across the Strait of Gibraltar.", unlocked: false },
-  { name: "Cairo",  x:  60,  y: -70, fact: "The pyramids near Cairo were already ancient when Cleopatra was alive.", unlocked: false },
-  { name: "Rio",    x: -50,  y:  120, fact: "Rio’s Christ the Redeemer statue is struck by lightning several times a year.", unlocked: false },
-  { name: "Mumbai", x: 130,  y:  -30, fact: "Mumbai is home to Bollywood, the largest film industry in the world by number of films produced.", unlocked: false },
+  { name: "Paris",  x: -5, y: -195, fact: "The Eiffel Tower expands slightly in summer heat.", unlocked: false },
+  { name: "Tangier",  x:  -10,  y: -140, fact: "On a clear day in Tangier, you can see Europe from Africa, Spain is only 9 miles away across the Strait of Gibraltar.", unlocked: false },
+  { name: "Cairo",  x:  100,  y: -120, fact: "The pyramids near Cairo were already ancient when Cleopatra was alive.", unlocked: false },
+  { name: "Rio",    x: -80,  y:  120, fact: "Rio’s Christ the Redeemer statue is struck by lightning several times a year.", unlocked: false },
+  { name: "Mumbai", x: 240,  y:  -70, fact: "Mumbai is home to Bollywood, the largest film industry in the world by number of films produced.", unlocked: false },
+  { name: "Bogotá", x: -220, y: 10, fact: "Bogotá sits over 8,600 feet above sea level, making it one of the highest capital cities in the world.", unlocked: false },
 ];
 
 function preload() {
-  globeImg = loadImage("img/earth.png");
+  globeImg = loadImage("img/full-globe.png");
+  planeImg = loadImage("img/plane.png");
 }
 
 function setup() {
@@ -23,7 +26,7 @@ function draw() {
   background(255);
 
   // Instructions
-  fill(0);
+  fill(255);
   textAlign(CENTER);
   textSize(20);
   text("Click locations on the globe to unlock travel facts!", width / 2, 40);
@@ -36,9 +39,8 @@ function draw() {
   let r = size / 2;
 
   // Draw globe
-  imageMode(CENTER);
-  image(globeImg, gx, gy, size, size);
-
+  imageMode(CORNER);
+image(globeImg, 0, 0, width, height);
   // Draw clickable dots
   for (let d of dots) {
     let px = gx + d.x;
@@ -50,11 +52,16 @@ function draw() {
       circle(px, py, 14);
     }
   }
+  fill(255);
+  textAlign(CENTER);
+  textSize(48);
+  text("Soaring Around the World", width / 2, 60);
+  textAlign(LEFT);
 
   // Star when complete
   if (unlockedCount === dots.length) {
     drawStar(gx, gy - r - 40, 15, 30, 5);
-    fill(0);
+    fill(255);
     textAlign(CENTER);
     text("Adventure Complete!", gx, gy - r - 15);
     textAlign(LEFT);
@@ -62,6 +69,9 @@ function draw() {
 
   // Fact box
   drawFactBox(currentFact);
+
+  imageMode(CENTER);
+image(planeImg, mouseX, mouseY, 50, 50);
 }
 
 function mousePressed() {
